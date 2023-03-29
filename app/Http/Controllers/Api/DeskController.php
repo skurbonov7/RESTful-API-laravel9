4a45bda479c8d258274bfcf6db5d6227a38b5767
Sorbon Kurbonov <sorbonkurbonov7@gmail.com>
2023-03-29 13:41:24 +0300

    receiving adding api

---
+++ app/Http/Controllers/Api/DeskController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\DeskResource;
+use App\Models\Desk;
+use Illuminate\Http\Request;
+
+class DeskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //return Desk::pluck();
+        return DeskResource::collection(Desk::with('lists')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $created_desk = Desk::create($request->all());
+
+        return  new DeskResource($created_desk);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Desk $desk)
+    {
+        // return Desk::where('id', $id)->get();
+        // return Desk::findOrFail($desk->id);
+        return new DeskResource(Desk::with('lists')->findOrFail($desk->id));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
